57438890974333997fd76badcfe469888df691ce
Piotr Mikosza <p.mikosza@outlook.com>
2017-06-01 21:07:01 +0200

    init commit

---
+++ client/Client.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client
+{
+    public class Client
+    {
+        const string dir = @"C:\From\";
+        private ServerReference.IServer wcfClient;
+
+        public Client()
+        {
+            this.createChannel(this.setAddress());
+            Console.WriteLine(this.wcfClient.sendMessage("login", getComputerName());
+            this.GetAllFiles(dir);
+            this.SetFileWatcher();
+            Console.ReadKey();
+        }
+        private ServerReference.IServer getWcfClient()
+        {
+            return wcfClient;
+        }
+
+        private EndpointAddress setAddress()
+        {
+            Console.WriteLine("Zaloguj się do Cloud Drive");
+            String adres = Console.ReadLine();
+
+            EndpointAddress myEndpoint = new EndpointAddress(adres);
+
+            //Dodać wyjątki że nie udało się zalogować
+            Console.WriteLine("Adres ustawiony");
+
+            return myEndpoint;
+        }
+
+        private ServerReference.IServer createChannel(EndpointAddress myEndpoint)
+        {
+            BasicHttpBinding myBinding = new BasicHttpBinding();
+
+            ChannelFactory<ServerReference.IServer> myChannelFactory = new ChannelFactory<ServerReference.IServer>(myBinding, myEndpoint);
+
+            wcfClient = myChannelFactory.CreateChannel();
+
+            //Dodać wyjątki że nie udało się utworzyć kanału
+            Console.WriteLine("Kanał utworzony");
+
+            return wcfClient;
+        }
+
+        private void closeChannel()
+        {
+            ((IClientChannel)this.wcfClient).Close();
+            Console.WriteLine("Kanał zamknięty");
+        }
+
+        private List<string> GetAllFiles(string sDirt)
+        {
+            List<string> files = new List<string>();
+
+            try
+            {
+                foreach (string file in Directory.GetFiles(sDirt))
+                {
+                    files.Add(file);
+                }
+                foreach (string fl in Directory.GetDirectories(sDirt))
+                {
+                    files.AddRange(GetAllFiles(fl));
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+            return files;
+        }
+
+        private void SendFiles(List<string> files)
+        {
+            files.ForEach(file =>
+            {
+                var bytes = File.ReadAllBytes(file);
+                wcfClient.SetFile(new ServerReference.FileContract
+                {
+                    Bytes = bytes,
+                    FilePath = file.Substring(dir.Length)
+                });
+            });
+        }
+        private void SetFileWatcher()
+        {
+            var watcher = new FileSystemWatcher();
+            watcher.Path = dir;
+            watcher.NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.FileName;
+            watcher.Filter = "*.*";
+            watcher.Created += (obj, e) =>
+            {
+                var files = this.GetAllFiles(dir);
+                this.SendFiles(files);
+            };
+            watcher.Changed += (obj, e) => { };
+            watcher.Deleted += (obj, e) => { };
+
+            watcher.EnableRaisingEvents = true;
+        }
+        private String getComputerName()
+        {
+            string name = Environment.MachineName;
+            return name;
+        }
+
+        static void Main(string[] args)
+        {
+            new Client();            
+        }
+
+    }
+}
---
+++ server/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.Runtime.Serialization;
+using System.IO;
+
+namespace server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost host = new ServiceHost(typeof(Server));
+            host.Open();
+            Console.WriteLine("Server is ready...");
+            Console.ReadKey();
+        }
+    }
+    [ServiceContract]
+    public interface IServer
+    {
+        [OperationContract]
+        String sendMessage(String command, String value);
+
+        [OperationContract]
+        void SetFile(FileContract fileContract);
+    }
+
+    [DataContract]
+    public class FileContract
+    {
+        [DataMember]
+        public byte[] Bytes { get; set; }
+
+        [DataMember]
+        public string FilePath { get; set; }
+
+        public string FileName
+        {
+            get
+            {
+                return Path.GetFileName(this.FilePath);
+            }
+        }
+    }
+
+    public class Server : IServer
+    {
+        const string dir = @"C:\To\";
+
+        public String sendMessage(String command, String value)
+        {
+            string response = "";
+            switch (command)
+            {
+                case "login":
+                    response = "Zalogowano pomyślnie !";
+                    Console.WriteLine("Użytkownik o nazwie " + value + " zalogował się do serwera");
+                    break;
+            }
+            return response;
+        }
+
+        public void SetFile(FileContract fileContract)
+        {
+            var path = Path.Combine(dir, fileContract.FilePath);
+            var dirname = Path.GetDirectoryName(path);
+
+            if (!Directory.Exists(dirname))
+            {
+                Directory.CreateDirectory(dirname);
+            }
+
+            File.WriteAllBytes(path, fileContract.Bytes);
+        }
+    }
+}
